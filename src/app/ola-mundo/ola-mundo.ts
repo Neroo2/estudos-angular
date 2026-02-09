b7d9edf09ec46bba11fe1c5b315f81b1c78ac5a3
Nero <91999620+Neroo2@users.noreply.github.com>
2026-02-09 09:26:57 -0300

    Initilized Angular Project

---
+++ src/app/ola-mundo/ola-mundo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ola-mundo',
+  imports: [],
+  templateUrl: './ola-mundo.html',
+  styleUrl: './ola-mundo.css',
+})
+export class OlaMundo {
+
+}
